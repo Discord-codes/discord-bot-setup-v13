bfcd4e3b85b39cb1cfed8be1ff50b0150b4d212e
Discord-Coder <89718630+Discord-codes@users.noreply.github.com>
2022-01-11 21:37:15 +0400

    Update index.js
    Ping pong command

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const { Client, Collection } = require('discord.js'); //define
 const { TOKEN, PREFIX } = require('./config.json'); //token and prefix
-const fs = require('fs') //define
-const ms = require("ms") //define
-const fetch = require('node-fetch') //define
-const client = new Client({
+const client = new Client({ //client intents
     intents: 32767
 });
-
-client.commands = new Collection();
-client.aliases = new Collection();
-client.categories = fs.readdirSync("./Commands");
-client.events = new Collection();
-
-module.exports = client;
-
-["Command", "Event"].forEach(handler => {
-  require(`./Structures/${handler}`)(client);
-});
+client.on('messageCreate', async message => {
+   if (message.content === "!ping") {
+    message.channel.send(`⏱| **${client.ws.ping}ms** Latency!`)
+}
+})
 
 client.login(config.token);
