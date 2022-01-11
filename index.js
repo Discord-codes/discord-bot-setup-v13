47bd9366150d4c861e97720d4d3c2095eb05045f
Discord-Coder <89718630+Discord-codes@users.noreply.github.com>
2022-01-11 21:33:42 +0400

    Update index.js
    New updated

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const express = require('express')
-const app = express();
-const port = 3000
 
-app.get('/', (req, res) => res.send('Bot online'))
-
-app.listen(port, () =>
-console.log(`Your app is listening a http://localhost:${port}`)
-); // for hosting 
 
 const { Client, Collection } = require('discord.js'); //define
 const { TOKEN, PREFIX } = require('./config.json'); //token and prefix
@@ -26,19 +18,6 @@ module.exports = client;
 
 ["Command", "Event"].forEach(handler => {
   require(`./Structures/${handler}`)(client);
-});
-
-client.once('ready', () => {
-  console.log(`[READY]: ${client.user.tag} is ready`)
-})
-
-process.on('beforeExit', async () => {
-    console.log(figlet(chalk.redBright('Shutting down...')));
-    await client.destroy();
-});
- 
-process.on('exit', async () => {
-    console.log(figlet(chalk.redBright('Shut down.')));
 });
 
 client.login(config.token);
