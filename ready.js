cca1739dd648cf678f0dcfb944055499fcb6849d
Discord-Coder <89718630+Discord-codes@users.noreply.github.com>
2022-01-11 21:32:23 +0400

    Update ready.js
    *Added hosting code ( use uptime for host )
    * Chalk package used to check , warn , before exit etc

--- ready.js
+++ ready.js
@@ -1,1 +1,26 @@
+const express = require('express')
+const app = express();
+const port = 3000
 
+app.get('/', (req, res) => res.send('Bot online'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+); // for hosting 
+
+const { Client, Collection } = require('discord.js'); //define
+const { TOKEN, PREFIX } = require('./config.json'); //token and prefix
+
+
+client.once('ready', () => {
+  console.log(`[READY]: ${client.user.tag} is ready`)
+}) // if client ready
+
+process.on('beforeExit', async () => {
+    console.log(figlet(chalk.redBright('Shutting down...')));
+    await client.destroy(); // if bot gonna be down
+});
+ 
+process.on('exit', async () => {
+    console.log(figlet(chalk.redBright('Shut down.')));
+}); // bot down
