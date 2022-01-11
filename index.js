d4de3c553ffa40b7559589ef72cf49279f244f02
Discord-Coder <89718630+Discord-codes@users.noreply.github.com>
2022-01-11 21:39:55 +0400

    Finished index.js
    Done

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const { Client, Collection } = require('discord.js'); //define
 const { TOKEN, PREFIX } = require('./config.json'); //token and prefix
-const client = new Client({ //client intents
-    intents: 32767
-});
-client.on('messageCreate', async message => {
-   if (message.content === "!ping") {
-    message.channel.send(`⏱| **${client.ws.ping}ms** Latency!`)
+const client = new Client({ intents: 32767 }); //intents
+client.on('messageCreate', async message => { //create message
+   if (message.content === "!ping") { //create command
+    message.channel.send(`⏱| **${client.ws.ping}ms** Latency!`) //command response
 }
 })
-
 client.login(config.token);
